94ba9657f9e261019534b4fcd388dd49b91920d9
ccgarcia1 <42050096+ccgarcia1@users.noreply.github.com>
2024-10-02 23:36:39 -0300

    adding if statement

--- server.py
+++ server.py
@@ -6,13 +6,15 @@ import requests
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def home():
+def current_year():
     today = datetime.date.today()
     year = today.year
+    return year
+@app.route('/')
+def home():
+
     random_number = random.randint(1,10)
-    return render_template("index.html", num=random_number, curr_year=year)
+    return render_template("index.html", num=random_number, curr_year=current_year())
 
 
 @app.route('/guess/<name>')
@@ -27,6 +29,12 @@ def guess(name):
     print(age)
     return render_template("guess.html", name=name, gender=gender, age=age)
 
+@app.route('/blog')
+def blog():
+    blog_url = 'https://api.npoint.io/c790b4d5cab58020d391'
+    blog_response = requests.get(blog_url)
+    all_posts = blog_response.json()
+    return render_template("blog.html", posts=all_posts, curr_year=current_year())
 
 if __name__ == "__main__":
     app.run(debug=True)

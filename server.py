c1ab459a27ad55e6e038797e0a17bf5a614ea3d0
ccgarcia1 <42050096+ccgarcia1@users.noreply.github.com>
2024-10-02 23:44:57 -0300

    adding if statement

--- server.py
+++ server.py
@@ -30,7 +30,7 @@ def guess(name):
     return render_template("guess.html", name=name, gender=gender, age=age)
 
 @app.route('/blog')
-def blog():
+def get_blog():
     blog_url = 'https://api.npoint.io/c790b4d5cab58020d391'
     blog_response = requests.get(blog_url)
     all_posts = blog_response.json()
